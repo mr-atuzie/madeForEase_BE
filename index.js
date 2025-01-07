d39f521e99513fa28ce0d5d892e88c6e0beb4845
rex atuzie <rexatuzie@gmail.com>
2025-01-07 13:09:04 +0100

    add frontend link

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: [
+      "https://madeforease.shop",
+      "http://localhost:3000",
+      "http://localhost:5173",
+    ],
     credentials: true,
   })
 );
